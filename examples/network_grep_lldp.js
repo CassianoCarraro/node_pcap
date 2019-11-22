86a55af84ab88fb4d4b2bb9d28f2c3dd612434a6
Daniel Balbinot <danielg2_champy@hotmail.com>
2019-11-22 04:03:01 -0300

    Inserção case 8

--- examples/network_grep_lldp.js
+++ examples/network_grep_lldp.js
@@ -5,6 +5,6 @@ console.log("Listening on " + pcap_session.device_name);
 
 pcap_session.on('packet', function (raw_packet) {
     var packet = pcap.decode.packet(raw_packet);
-    console.log(packet.toString());
-    console.log(packet);
+    // console.log(packet.toString());
+    // console.log(packet);
 });
